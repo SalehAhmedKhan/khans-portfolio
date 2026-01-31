8b403925152a60b7bdd51f36bbecd862ea14bee2
Saleh Ahmed Khan <salehahmedkhan1998@gmail.com>
2026-01-30 23:36:39 -0500

    Create prophets.js

---
+++ islamics/js/tabs/prophets.js
@@ -0,0 +1,5 @@
+export const TAB_TITLE = "Stories of the Prophets";
+
+export const PANELS = [
+  { figure: "tab_images/prophets/01.jpg", header: "Panel 1", text: "Replace with your content." },
+];
