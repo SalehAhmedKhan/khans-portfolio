ee81715d32385020f65778310115795deaeede60
Saleh Ahmed Khan <salehahmedkhan1998@gmail.com>
2026-01-30 23:36:01 -0500

    Create arabic.js

---
+++ islamics/js/tabs/arabic.js
@@ -0,0 +1,5 @@
+export const TAB_TITLE = "Essential Arabics for Every Muslim";
+
+export const PANELS = [
+  { figure: "tab_images/arabic/01.jpg", header: "Panel 1", text: "Replace with your content." },
+];
