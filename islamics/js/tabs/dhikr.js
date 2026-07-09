c51e61d8ca7561fa284c6f689f6354f08295a0a6
Saleh Ahmed Khan <salehahmedkhan1998@gmail.com>
2026-07-08 23:18:24 -0400

    Change TAB_TITLE to 'The Foundations of Faith'

--- islamics/js/tabs/dhikr.js
+++ islamics/js/tabs/dhikr.js
@@ -1,4 +1,4 @@
-export const TAB_TITLE = "Daily Dhikr & Du'a";
+export const TAB_TITLE = "The Foundations of Faith";
 
 export const PANELS = [
   { figure: "tab_images/dhikr/01.jpg", header: "Panel 1", text: "Replace with your content." },
